b97ea2ecc73eec20d8ce5d60ffcbf5972ad1cdb2
Didi0dum <Deedee.vasileva@gmail.com>
2024-02-08 22:19:14 +0200

    Homework 1 test commit

---
+++ HW1_Dilyana_Vasileva_9a 2/HW1_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Row and col indexes were swapped in for cycle 
+
+int main() {
+
+  int arr[2][3] = {{1, 7, 6}, {5, 2, 8}};
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (arr[i][j] % 2 == 1) {
+        printf("%d ", arr[i][j]);
+      }
+    }
+  }
+
+  return 0;
+}
